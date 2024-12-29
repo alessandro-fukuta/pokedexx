c7a0ccba1b338f98dab0385fdcc76e7c48742dd5
Alessandro Fukuta <fukuta2003@hotmail.com>
2024-12-29 19:54:35 -0300

    first_version

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div id="app">
+    <div class="column is-half is-offset-one-quarter">
+      <img id="logo" src="./assets/logo_fukuta.png"/>
+      
+      <p class="title is-2">Pokedexx</p>
+      <p class="subtitle is-5">Consumindo API com NodeJS + Vue + Bulma + Axios + PokeAPI</p>
+      <input class="input is-primary is-rounded" type="text" v-model="busca" placeholder="buscar pokemon pelo nome">
+      <hr>
+      <div v-for="(poke,index) in resultadoBusca" v-bind:key = "index">
+        <Pokemon :name="poke.name" :url="poke.url" :num="index+1"></Pokemon>
+    </div>
+
+  </div>
+   
+ </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios';
+import Pokemon from './components/Pokemon';
 export default {
   name: 'App',
+  data() {
+    return {
+      pokemons: [],
+      busca: ''
+    }
+  },
+  created: function() {
+   
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res => {
+      console.log("Pegou a lista dos pokemons");
+      this.pokemons = res.data.results;
+    })
+
+  },
   components: {
-    HelloWorld
+    Pokemon
+  },
+  computed: {
+    resultadoBusca: function() {
+      if(this.busca == '' || this.busca == ' ') {
+        return this.pokemons;
+      } else {
+        return this.pokemons.filter( pokemon => pokemon.name == this.busca)
+      }
+    }
   }
 }
 </script>
 
 <style>
+body {
+  background-color: #f1f5f8;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +61,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#logo {
+  width: 30%;
+}
+
+#buscaBtn {
+  margin-top:2%;
 }
 </style>
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,85 @@
+<template>
+    <div id="pokemon">
+
+        <div class="card">
+                <div class="card-image">
+                    <figure>
+                    <img :src="currentImg" width="15%" />
+                    </figure>
+                </div>
+                <div class="card-content">
+                    <div class="media">
+                    <div class="media-content">
+                        <p class="title is-4">{{ num }} - {{ name }}</p>
+                        <p class="subtitle is-6">{{ pokemon.type }}</p>
+                    </div>
+                    </div>
+
+                    <div class="content">
+                        <button @click = "mudarSprite" class="button is-medium is-fullwidth">Mudar Sprite</button>
+                    </div>
+                </div>
+</div>
+
+
+
+
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    created: function() {
+        axios.get(this.url).then(res => {
+            this.pokemon.type = res.data.types[0].type.name;
+            this.pokemon.front = res.data.sprites.front_default;
+            this.pokemon.back = res.data.sprites.back_default;
+            this.currentImg = this.pokemon.front;
+            console.log(this.pokemon);
+        })
+    },
+    
+    props: {
+        num: Number,
+        name: String,
+        url: String
+    },
+    
+    data() {
+        return {
+            isFront: true,
+            currentImg: '',
+            pokemon: {
+                type: '',
+                front: '',
+                back: ''
+            }
+        }
+    },
+
+    methods: {
+        mudarSprite: function() {
+            if(this.isFront){
+                this.isFront = false;
+                this.currentImg = this.pokemon.back;
+            } else {
+                this.isFront = true;
+                this.currentImg = this.pokemon.front;
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+#pokemon {
+    margin-top:2%;
+   
+    
+}
+
+
+</style>
